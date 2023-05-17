ff3f63b55744d88463dbec40a606b8ee26878b3a
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-05-17 11:09:56 +0100

    Start to add userinput

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let container = document.querySelector('.container');
+let gridSquares = document.getElementsByClassName('gridSquare');
+
+let button = document.querySelector('button');
+button.addEventListener('click', userPrompt);
+
+let userInput = 0
+
+function userPrompt() {
+    userInput = prompt("How many squares wide/high would you like?")
+    if (userInput > 100 || userInput < 0) {
+        alert("Error, value must be less than 100 and over 0")
+        userInput = 0
+    }
+}
+
 
 function createRow() {
     let newContainer = document.createElement('div');
     newContainer.className = "newContainer";
     container.appendChild(newContainer);
 
-    for (let i = 1; i < 17; i++) {
+    for (let i = 1; i < (userInput + 1); i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "gridSquare";
         newContainer.appendChild(newDiv);
@@ -14,7 +29,7 @@ function createRow() {
 
 }
 
-for (let j = 1; j < 17; j++) {
+for (let j = 1; j < (userInput + 1); j++) {
     createRow();
 }
 
@@ -22,8 +37,8 @@ function hover(e) {
     e.target.style.backgroundColor = "grey";    
 }
 
-let gridSquares = document.getElementsByClassName('gridSquare');
-
 for (square of gridSquares) {
     square.addEventListener('mouseover', hover)
 }
+
+
