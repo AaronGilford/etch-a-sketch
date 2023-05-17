4df53422fabbcb2363adbf438bb5c590d008854e
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-05-17 10:44:12 +0100

    Add hover effect to grid

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createRow() {
     let newContainer = document.createElement('div');
     newContainer.className = "newContainer";
     container.appendChild(newContainer);
+
     for (let i = 1; i < 17; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "gridSquare";
@@ -15,4 +16,14 @@ function createRow() {
 
 for (let j = 1; j < 17; j++) {
     createRow();
+}
+
+function hover(e) {
+    e.target.style.backgroundColor = "grey";    
+}
+
+let gridSquares = document.getElementsByClassName('gridSquare');
+
+for (square of gridSquares) {
+    square.addEventListener('mouseover', hover)
 }
