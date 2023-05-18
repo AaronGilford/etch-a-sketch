55263854e7336016012efe71cdb012615412495a
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-05-18 09:49:59 +0100

    Fix user prompt, remove dead space

--- script.js
+++ script.js
@@ -4,23 +4,38 @@ let gridSquares = document.getElementsByClassName('gridSquare');
 let button = document.querySelector('button');
 button.addEventListener('click', userPrompt);
 
-let userInput = 0
+let userInput = 16
+createGrid();
 
 function userPrompt() {
     userInput = prompt("How many squares wide/high would you like?")
     if (userInput > 100 || userInput < 0) {
         alert("Error, value must be less than 100 and over 0")
-        userInput = 0
+    } else {
+        removeElementsByClassName("gridSquare");
+        removeElementsByClassName("newContainer");
+        console.log(gridSquares);
+        createGrid();
+        for (square of gridSquares) {
+            square.addEventListener('mouseover', hover)
+        }
+        
     }
 }
 
+function removeElementsByClassName(className) {
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
 function createRow() {
     let newContainer = document.createElement('div');
     newContainer.className = "newContainer";
     container.appendChild(newContainer);
 
-    for (let i = 1; i < (userInput + 1); i++) {
+    for (let i = 0; i < userInput; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "gridSquare";
         newContainer.appendChild(newDiv);
@@ -28,11 +43,11 @@ function createRow() {
     
 
 }
-
-for (let j = 1; j < (userInput + 1); j++) {
-    createRow();
+function createGrid() {
+    for (let j = 0; j < userInput; j++) {
+        createRow();
+    }
 }
-
 function hover(e) {
     e.target.style.backgroundColor = "grey";    
 }
@@ -40,5 +55,4 @@ function hover(e) {
 for (square of gridSquares) {
     square.addEventListener('mouseover', hover)
 }
-
 
